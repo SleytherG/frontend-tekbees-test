86df50b895cebde6c8b11dc0478c2f87ff44ce24
sleytherg <sleyther.calsin@cloudcomputing.com.pe>
2023-07-06 12:18:50 -0500

    First Commit

---
+++ src/app/models/Auth.ts
@@ -0,0 +1,11 @@
+export interface Login {
+  username: string,
+  password: string
+}
+
+export interface Register {
+  nombres: string,
+  apellidos: string,
+  username: string,
+  password: string
+}
